3de9fa1b7903ff99141d2c30669bb304eefccedc
Scott Feeney <graue@oceanbase.org>
2006-11-19 16:18:17 -0500

    Don't define int16_t and int32_t on Windows.
    darcs-hash:20061119211817-bbafa-46eb6d1d6a2db50e35cb11c2ac308cd25d77638d

--- types.h
+++ types.h
@@ -1,8 +1,7 @@
 #ifdef __WIN32
 typedef unsigned short u_int16_t;
 typedef unsigned long u_int32_t;
-typedef short int16_t;
-typedef long int32_t;
+/* MinGW knows about int16_t and int32_t, though. */
 #define letoh16(x) (x) /* little endian only */
 #define htole16(x) (x) /* little endian only */
 #define letoh32(x) (x) /* little endian only */
