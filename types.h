b788b98bcb65423e99bb0cd797c29e63af87c0c4
Scott Feeney <graue@oceanbase.org>
2006-11-19 05:57:58 -0500

    Add int16_t, int32_t.
    darcs-hash:20061119105758-bbafa-db92425abba6c82da64d684398cf1871c1aef9ae

--- types.h
+++ types.h
@@ -1,6 +1,8 @@
 #ifdef __WIN32
 typedef unsigned short u_int16_t;
 typedef unsigned long u_int32_t;
+typedef short int16_t;
+typedef long int32_t;
 #define letoh16(x) (x) /* little endian only */
 #define htole16(x) (x) /* little endian only */
 #define letoh32(x) (x) /* little endian only */
