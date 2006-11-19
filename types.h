f4401ddc7a8f38139f7c349f2a8dd6b04f67e090
Scott Feeney <graue@oceanbase.org>
2006-11-19 16:38:11 -0500

    Adjust for MinGW weirdness: it knows int32_t only.
    darcs-hash:20061119213811-bbafa-a9f26a43f4248fff52cf33c719b75b84543a024d

--- types.h
+++ types.h
@@ -1,7 +1,8 @@
 #ifdef __WIN32
 typedef unsigned short u_int16_t;
 typedef unsigned long u_int32_t;
-/* MinGW knows about int16_t and int32_t, though. */
+typedef short int16_t;
+/* MinGW knows about int32_t but _not_ those above three, apparently. */
 #define letoh16(x) (x) /* little endian only */
 #define htole16(x) (x) /* little endian only */
 #define letoh32(x) (x) /* little endian only */
