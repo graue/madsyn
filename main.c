2a9d1adb469bb9e8b001c7b586e82522014d3802
Scott Feeney <graue@oceanbase.org>
2006-11-19 14:16:41 -0500

    Add options for length and number of mutations.
    darcs-hash:20061119191641-bbafa-14fe284d03e75b583d9c14014e5bdfe84dd10b7d

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> /* isatty */
+#include <unistd.h> /* isatty, getopt */
 #include "err.h"
 #include "binary.h"
 #include "types.h"
@@ -8,28 +8,54 @@
 #include "protos.h"
 
 #define SAMPRATE 44100
-#define LEN (5*SAMPRATE)
-#define MUTATIONS 3
+#define DEF_LEN 5000.0f /* default 5 sec */
+#define DEF_MUTATIONS 3
 
-int main(void)
+#define MS_TO_SAMPLES(l) ((int)((l) * SAMPRATE / 1000.0f))
+
+static void usage(void)
+{
+	fprintf(stderr, "usage: madsyn [-l len_in_ms] [-m num_mutations]\n");
+	exit(EXIT_FAILURE);
+}
+
+extern int optind;
+extern char *optarg;
+
+int main(int argc, char *argv[])
 {
 	tree_t *tree;
 	tree_t *mutant;
-	int ix;
+	int ch, ix;
+	int numsamples = MS_TO_SAMPLES(DEF_LEN);
+	int nummutations = DEF_MUTATIONS;
+
+	while ((ch = getopt(argc, argv, "l:m:")) != -1)
+	{
+		if (ch == 'l') /* length in ms */
+		{
+			numsamples = MS_TO_SAMPLES(strtod(optarg, NULL));
+			if (numsamples < 0) numsamples = 0;
+		}
+		else if (ch == 'm') /* number of mutations to do */
+			nummutations = atoi(optarg);
+		else usage();
+	}
+	if (argc - optind > 0) usage();
 
 	if (isatty(STDOUT_FILENO))
 		errx(1, "I won't write junk onto your terminal.");
 
 	tree = readtree(stdin);
 	printtree(tree, stderr);
-	play(tree, stdout, LEN, SAMPRATE);
+	play(tree, stdout, numsamples, SAMPRATE);
 
-	for (ix = 0; ix < MUTATIONS; ix++)
+	for (ix = 0; ix < nummutations; ix++)
 	{
 		mutant = copytree(tree);
 		mutate(mutant);
 		printtree(mutant, stderr);
-		play(mutant, stdout, LEN, SAMPRATE);
+		play(mutant, stdout, numsamples, SAMPRATE);
 		destroytree(mutant);
 	}
 
