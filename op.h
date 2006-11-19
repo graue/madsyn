1c44345736b702cead6a305f090ba9a08b3e383e
Scott Feeney <graue@oceanbase.org>
2006-11-19 04:57:07 -0500

    Use a typedef for the run function pointer.
    darcs-hash:20061119095707-bbafa-8dbb68b00ebe9828dd039d4f5335c7f3ab6a2453

--- op.h
+++ op.h
@@ -1,10 +1,12 @@
 /* Defining operators. */
 
+typedef void (*runptr)(const frame_t *in, frame_t *out, void *state);
+
 typedef struct
 {
 	int numinputs;
 	int numoutputs;
-	void (*run)(const frame_t *in, frame_t *out, void *state);
+	runptr run;
 	void *(*init)(void); /* NULL if no state */
 	const char *name;
 } opdef_t;
