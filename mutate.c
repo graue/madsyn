67f709db622c1b2ecbffa7ef8ade0c970211536d
Scott Feeney <graue@oceanbase.org>
2006-11-19 11:50:30 -0500

    Preserve parent links when mutating.
    darcs-hash:20061119165030-bbafa-a1a774d45b8576dc8a0a2d603c94c7c7056a021a

--- mutate.c
+++ mutate.c
@@ -13,21 +13,48 @@ static treenode_t *randomnode(treenode_t *start, int depth)
 	return randomnode(start->inputs[rnd(start->op->numinputs)], depth-1);
 }
 
+/* Fix up the parent links under node, after a mutation fubars them. */
+static void fixupparents(treenode_t *node)
+{
+	int ix;
+	if (node->op == NULL) return;
+	for (ix = 0; ix < node->op->numinputs; ix++)
+	{
+		fixupparents(node->inputs[ix]);
+		node->inputs[ix]->parent = node;
+	}
+}
+
 /* BEGIN MUTATIONS */
+
+#define PERSISTENCE 50
 
 void mut_swapsubtrees(treenode_t *node)
 {
 	treenode_t extra;
 	treenode_t *one, *two;
+	int tries = PERSISTENCE;
 
 	if (node->depth <= 0) return;
-	one = randomnode(node, rnd(node->depth));
-	two = randomnode(node, rnd(node->depth));
-	if (one == two) return;
+	do
+	{
+		one = randomnode(node, rnd(node->depth));
+		two = randomnode(node, rnd(node->depth));
+
+		/*
+		 * Note that we make sure the two nodes swapped
+		 * are at the same depth, so one can't be a
+		 * subtree of the other (that would be pretty
+		 * bad):
+		 */
+	} while (tries-- > 0 && (one == two || one->depth != two->depth));
+	if (tries == 0) return; /* didn't work out */
 
 	memcpy(&extra, one, sizeof extra);
 	memcpy(one, two, sizeof *one);
 	memcpy(two, &extra, sizeof *two);
+
+	fixupparents(node);
 }
 
 /* END MUTATIONS (although they are listed again below */
