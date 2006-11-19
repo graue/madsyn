40983d31eb402247b563ee93d44087d98f9a728c
Scott Feeney <graue@oceanbase.org>
2006-11-19 05:52:32 -0500

    Add crutch file with types & endianness.
    darcs-hash:20061119105232-bbafa-9a1b8879a264869666e6eaf3b7a0d70460331a49

---
+++ types.h
@@ -0,0 +1,10 @@
+#ifdef __WIN32
+typedef unsigned short u_int16_t;
+typedef unsigned long u_int32_t;
+#define letoh16(x) (x) /* little endian only */
+#define htole16(x) (x) /* little endian only */
+#define letoh32(x) (x) /* little endian only */
+#define htole32(x) (x) /* little endian only */
+#else
+#include <sys/types.h>
+#endif
