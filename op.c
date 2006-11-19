5bfa8c7259120bfbb96dc4af967e46cd0ba262c0
Scott Feeney <graue@oceanbase.org>
2006-11-19 04:45:12 -0500

    Add cos, log, fabs, swap, pan functions.
    darcs-hash:20061119094512-bbafa-31eb96fc3478cb75c4d37d536521bcc3da0f7cd3

--- op.c
+++ op.c
@@ -14,10 +14,32 @@ static void op_ ## name (const frame_t *in, frame_t *out, void *state)	\
 }
 
 SIMPLE_FUNC(sin)
+SIMPLE_FUNC(cos)
+SIMPLE_FUNC(log)
+SIMPLE_FUNC(fabs)
+
+static void op_swap(const frame_t *in, frame_t *out, void *state)
+{
+	(void)state;
+	out[0][0] = in[1][0];
+	out[0][1] = in[1][1];
+	out[1][0] = in[0][0];
+	out[1][1] = in[0][1];
+}
+
+static void op_pan(const frame_t *in, frame_t *out, void *state)
+{
+	smp_t angle;
+	(void)state;
+	angle = M_PI/2 * ((in[1][0] + in[1][1]) * 0.5f - 0.5f);
+	out[0][0] = in[0][0] * cos(angle) - sin(angle);
+	out[0][1] = in[0][1] * cos(angle) + sin(angle);
+}
 
 /*typedef struct
 {
 	int numinputs;
+	int numoutputs;
 	void (*run)(const frame_t *in, frame_t *out, void *state);
 	void *(*init)(void); // NULL if no state
 	const char *name;
@@ -26,5 +48,10 @@ SIMPLE_FUNC(sin)
 
 opdef_t ops[] =
 {
-	{ 1, op_sin, NULL, "sin" }
+	{ 1, 1, op_sin, NULL, "sin" },
+	{ 1, 1, op_cos, NULL, "cos" },
+	{ 1, 1, op_log, NULL, "log" },
+	{ 1, 1, op_fabs, NULL, "abs" },
+	{ 2, 2, op_swap, NULL, "swap" },
+	{ 1, 2, op_pan, NULL, "pan" },
 };
--- op.h
+++ op.h
@@ -3,6 +3,7 @@
 typedef struct
 {
 	int numinputs;
+	int numoutputs;
 	void (*run)(const frame_t *in, frame_t *out, void *state);
 	void *(*init)(void); /* NULL if no state */
 	const char *name;
