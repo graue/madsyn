d50adfb1763e6b58cdb89ca77d8019521e3b3fd3
Scott Feeney <graue@oceanbase.org>
2006-11-19 10:56:28 -0500

    Add a proto for copytree.
    darcs-hash:20061119155628-bbafa-e03b92a0d1dd6253bfe13b6df30e14c9dccbb3e8

--- protos.h
+++ protos.h
@@ -2,3 +2,4 @@ void play(tree_t *tree, FILE *out, int numsamples, int samprate);
 tree_t *readtree(FILE *fp);
 void printtree(tree_t *tree, FILE *fp);
 void destroy(treenode_t *node);
+tree_t *copytree(const tree_t *tree);
